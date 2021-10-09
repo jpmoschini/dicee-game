457d611c1839db5d1c06c6ecb782d9216a13daaa
Juan Pablo Moschini <63172999+jpmoschini@users.noreply.github.com>
2021-10-08 23:53:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+//Para generar un random y modificar la imagen 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//Para generar un random y modificar la imagen 2
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+//Condicionales para mostrar ganador
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
